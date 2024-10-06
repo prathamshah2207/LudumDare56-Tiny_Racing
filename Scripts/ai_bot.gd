08623987b5ba05f7136227cb2fbb72b41b9fdf82
mvr659 <mvr659@usask.ca>
2024-10-05 18:10:10 -0600

    Pratham: Added navigationAgent to the bot that can detect the navigatable path it can move on on the track

--- Scripts/ai_bot.gd
+++ Scripts/ai_bot.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
-@export var speed := 500.0
+@export var speed := 400.0
+@export var accel := 10.0
 @export var boost_speed := 800.0
 var boost_available := false
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
 
 func _physics_process(delta: float) -> void:
-	move_bot(delta)
-
-func move_bot(delta):
-	var direction = Vector2.ZERO
+	var direction = Vector2()
+	nav.target_position = get_global_mouse_position()
 	
-	direction.y = -1
+	direction = nav.get_next_path_position() - global_position
+	print(direction.normalized())
+	#direction = direction.normalized()
+	#velocity = velocity.lerp(direction*speed, accel*delta)
 	
-	if boost_available:
-		direction *= boost_speed
-	else:
-		direction *= speed
-	velocity = direction
+	#if boost_available:
+		#direction *= boost_speed
+	#else:
+		#direction *= speed
+	#velocity = direction
 	move_and_slide()
