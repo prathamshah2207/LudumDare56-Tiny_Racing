3a9491e0039a7985d993f7442c19682c3a1a040f
Abhi <abhisavalia2745@gmail.com>
2024-10-07 08:35:47 -0600

    Changes the player movement to be more smooth and the camera follows the player with the camera zoom at start to have a look at map initially

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -1,36 +1,40 @@
 extends RigidBody2D
 
 # Speed Variables
-@export var base_speed := 100.0  # Normal driving speed
-@export var turn_speed := 3.0    # Turning speed
+@export var turn_speed := 3.3    # Turning speed
 
 # Current speed (will vary between base and boost speed)
-var speed := 250.0
+var speed := 350.0
 
 func _ready():
-	position = Vector2(-312, 142)
+	pass
 
 func _physics_process(delta):
 	# Reset velocity before applying new one
-	linear_velocity = Vector2.ZERO
-	angular_velocity = 0
+
+	var is_moving = false
+	var reverse = false
 
 	# Apply velocity to move forward or backward
 	if Input.is_action_pressed("move_up"):
+		is_moving = true
 		# Forward velocity, using the car's facing direction
-		linear_velocity += Vector2(0, -speed).rotated(rotation)
-	elif Input.is_action_pressed("move_down"):
+		linear_velocity = Vector2(0, -speed).rotated(rotation)
+		$Sprite2D.play("smoke")
+	if Input.is_action_pressed("move_down"):
 		# Backward velocity, using half the speed for reverse
-		linear_velocity += Vector2(0, speed).rotated(rotation)
-
-	# Rotate the car
-	if Input.is_action_pressed("move_left"):
-		angular_velocity += -turn_speed
-	if Input.is_action_pressed("move_right"):
-		angular_velocity += turn_speed
-	# Remove or comment out any code that modifies the camera rotation
-	# This ensures the camera remains fixed and does not rotate with the car
-
-	# For example, remove or comment out:
-	# $Camera2D.rotation = last_camera_rotation
-	# Or any other lines that set $Camera2D.rotation
+		is_moving = true
+		reverse = true
+		linear_velocity = Vector2(0, speed).rotated(rotation) 
+	# Rotate the car only if it is moving
+	if is_moving:
+		if Input.is_action_pressed("move_left"):
+			if reverse:
+				angular_velocity = turn_speed
+			else:
+				angular_velocity = -turn_speed
+		if Input.is_action_pressed("move_right"):
+			if reverse:
+				angular_velocity = -turn_speed
+			else:
+				angular_velocity = turn_speed
